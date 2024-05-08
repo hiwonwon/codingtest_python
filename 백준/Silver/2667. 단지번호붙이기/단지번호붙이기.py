743b1a2088199c34a86b2fd95cd8848cb8ce2022
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2024-05-08 11:28:40 +0900

    [Silver I] Title: 단지번호붙이기, Time: 64 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,46 +1,51 @@
-import sys
 from collections import deque
 
 
 
+def dfs(graph,a,b):
+    q = deque()
+    q.append((a,b))
 
-def bfs(graph, x,y):
+    home_cnt = 0
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
+    home_cnt+=1
+    graph[a][b] +=1
 
-    dx = [0, 0, 1, -1]
-    dy = [1, -1, 0, 0]
-    n = len(graph)
-    #apt = 0는 단지별 아파트 개수
-    apt = 1
-    queue = deque()
-    queue.append((x,y))
-    graph[x][y] = 0 
-
-    while queue:
-        x,y = queue.popleft()
-        
+    
+    while(q):
+        x,y = q.popleft()
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if 0<=nx<n and 0<=ny<n and graph[nx][ny] == 1:
-                apt +=1
-                #카운트 한 곳은 0으로 바꾸기
+            nx = dx[i] + x
+            ny = dy[i] + y
+            if 0 <= nx < n and 0<= ny < n and graph[nx][ny] == 1:
+                q.append ((nx,ny))
+                home_cnt+=1
+                #visited 대신 값을 변경하여 방문여부 체크
                 graph[nx][ny] = 0
-                queue.append((nx,ny))
-    return apt
+    return home_cnt
 
 n = int(input())
+
 graph = []
 for i in range(n):
     graph.append(list(map(int, input())))
 
-ans = []
 
+home_list = []
 for i in range(n):
     for j in range(n):
-        if graph[i][j]==1:
-            ans.append(bfs(graph, i, j))
+        if graph[i][j] == 1:
+            home_list.append(dfs(graph, i,j))
 
-print(len(ans))
-for i in sorted(ans):
-    print(i)
+print(len(home_list))
+
+
+for h in sorted(home_list):
+    print(h)
+
+
+
+
+
+        
