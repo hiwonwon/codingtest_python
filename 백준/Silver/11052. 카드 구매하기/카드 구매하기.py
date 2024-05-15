ac4141d8e2bbe1d83c48b0162c986e2a81de7e6d
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2024-05-15 17:28:14 +0900

    [Silver I] Title: 카드 구매하기, Time: 108 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -1,14 +1,22 @@
+N = int(input())
 
+p = {}
+inp = input().split()
+for i, elm in enumerate(inp):
+    p[i+1] = int(elm)
 
-n = int(input())
-cards = [0] +list(map(int,input().split()))
+a = {}
 
+for i in range(1, N+1):
+    if i == 1:
+        a[1] = p[1]
+    elif i == 2:
+        a[2] = max(p[2], a[1] + a[1])
+    else:
+        k = [p[i]]
+        for j in range(1, i//2+1):
+            k.append(a[j]+a[i-j])
+        a[i] = max(k)
 
-dp = [0]*(n+1)
-
-for i in range(1,n+1):
-    for j in range(1,i+1):
+print(a[N])
         
-        dp[i] = max(dp[i-j]+cards[j],dp[i])
-
-print(dp[n])
