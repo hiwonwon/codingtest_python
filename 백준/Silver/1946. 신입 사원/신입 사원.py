d02a8b782c2a48faef343c5699a9b30e8d13a016
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2024-06-07 13:27:43 +0900

    [Silver I] Title: 신입 사원, Time: 2860 ms, Memory: 229048 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.py
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -1,14 +1,32 @@
 import sys
-t = int(input())
+sys.setrecursionlimit(10000)
 
-for _ in range(t):
-    n = int(input())
-    arr = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-    arr.sort()
-    max = arr[0][1]
-    cnt = 1
-    for i in range(1,n):
-        if arr[i][1]<max:
-            cnt+=1
-            max = arr[i][1]
-    print(cnt)
+# M, N, K = map(int, sys.stdin.readline().split())
+
+
+a = int(input())
+
+result = []
+for i in range(a):
+    b = int(input())
+    all_li = []
+    for j in range(b):
+        all_li.append(list(map(int, sys.stdin.readline().split())))
+    
+    all_li.sort(key=lambda x:(x[0]))
+    # print(all_li)
+    sum = 0
+    t_li = [p[1] for p in all_li]
+    minnum = 1000000000000
+    for i, t  in enumerate(t_li):
+
+        if minnum < t:
+            sum+=1
+        
+        minnum = min(minnum, t)
+    result.append(len(all_li)-sum)
+
+for i in result:
+    print(i)
+        
+    
