028c62467e9448711d17e503e08a9b80208c5650
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2024-06-18 17:09:16 +0900

    [level 2] Title: 피보나치 수, Time: 453.57 ms, Memory: 439 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -0,0 +1,12 @@
+def solution(n):
+    answer = 0
+    fibo = [0]*(n+1)
+    fibo[0] = 0
+    fibo[1] =1
+
+    
+    for i in range(2,n+1):
+        fibo[i] = fibo[i-1]+fibo[i-2]
+    # print(fibo)
+    answer = fibo[n] % 1234567
+    return answer
