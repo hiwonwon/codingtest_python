8ae1a4d16ef42282af446d948a420208c97375c7
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2024-06-20 14:22:30 +0900

    [Silver I] Title: 카잉 달력, Time: 976 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/6064. 카잉 달력/카잉 달력.py
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.py
@@ -1,18 +1,13 @@
 import sys
-
 input = sys.stdin.readline
 
-t = int(input())
-for _ in range(t):
-    M, N, x, y = map(int, input().split())
+def calender(m,n,x,y):
+    for i in range(x,m*n+1,m):
+        if (i-y)%n==0:
+            return i
+    return -1
 
-    k = x
-    while k <= M * N:
-        # print(k)
-        if (k - y) % N == 0:
-            break
-        k += M
-    if k > M * N:
-        k = -1
-    print(k)
-    # print("-----")
+t = int(input().rstrip())
+for _ in range(t):
+    m,n,x,y = map(int,input().split())
+    print(calender(m,n,x,y))
