85ae8770e6d9d2c978f1b02bfa6a30f94b0d2c44
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2024-06-23 15:40:29 +0900

    [Silver I] Title: 영역 구하기, Time: 56 ms, Memory: 32604 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.py
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -1,51 +1,49 @@
-m, n, k = map(int, input().split())
+import sys
+sys.setrecursionlimit(10**6)
 
-#비어있는 좌표판을 만들어줌
-background = [[0]*n for _ in range(m)]
-
-klist=[]
-for _ in range(k):
-   y1,x1,y2,x2 = map(int, input().split())
-   #사각형이 그려지는 부분 체크 해줌
-   for i in range(x1,x2):
-    for j in range(y1,y2):
-      background[i][j] = 1  
-#print(background)
-
-from collections import deque
-ans = []
-
-def bfs(a,b):
-  q = deque()
-  dx = [-1,1,0,0]
-  dy = [0,0,-1,1]
-  q.append((a,b))
-  background[a][b] =1
-  cnt = 1
-  while(q):
-    x,y = q.popleft()
-    
+def DFS(x,y):
+    global cnt
     for i in range(4):
-      nx = dx[i] + x
-      ny = dy[i] + y
-      if 0<= nx <m and 0<= ny <n and background[nx][ny] == 0:
-        background[nx][ny] = 1
-        q.append((nx,ny))
-        cnt+=1
-  ans.append(cnt)
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0<=nx<m and 0<=ny<n and visited[nx][ny] == 0 and maps[nx][ny] == 0:
+            visited[nx][ny] = 1
+            cnt +=1
+            DFS(nx,ny)
+
+if __name__ == "__main__":
 
 
+    count = 0
+    cnt = 0
+    result = []
 
-for i in range(m):
-  for j in range(n):
-    if background[i][j]==0:
-      bfs(i,j)
-      
+    input = sys.stdin.readline
+    m, n, k = map(int,input().split())
+    maps = [[0]*n for _ in range(m)]
+    visited = maps.copy()
 
-print(len(ans))
-ans.sort()
-for a in ans:
-  print(f'{a} ', end='')
+    dx = [-1, 0, 1, 0]
+    dy = [0, 1, 0, -1]
+
+    for i in range(k):
+        ly, lx, ry, rx = map(int, input().split())
+        for j in range(lx, rx):
+            for k in range(ly, ry):
+                maps[j][k] = 1
 
 
-
+    
+    for i in range(m):
+        for j in range(n):
+            if maps[i][j] == 0 and visited[i][j] == 0:
+                count +=1
+                cnt +=1
+                visited[i][j] = 1
+                DFS(i,j)
+                result.append(cnt)
+                cnt = 0
+    
+    print(count)
+    result.sort()
+    print(' '.join(map(str,result)))
