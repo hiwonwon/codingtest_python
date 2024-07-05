5fedeafe72ca79f920f8e4fd833b29e08fb98c11
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2024-07-05 23:40:45 +0900

    [Silver I] Title: 그림, Time: 248 ms, Memory: 119764 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -1,36 +1,39 @@
 from collections import deque
-n,m = map(int, input().split())
-paint = []
-for _ in range(n):
-    paint.append(list(map(int, input().split())))
 
-visited = [[0]*m for _ in range(n)]
-
-def bfs (x,y):
-    q =deque()
-    q.append((x,y))
-    dx= [-1,1,0,0]
-    dy = [0,0,1,-1]
+def bfs(i, k, visited):
+  q = deque()
+  q.append((i, k))
+  visited[i][k] = 1
+  size = 1
+  dx = [-1, 1, 0, 0]
+  dy = [0, 0, -1 ,1]
+  while q:
+    x, y = q.popleft()
     visited[x][y] = 1
-    cnt = 1
-    while(q):
-        x,y = q.popleft()
-        for i in range(4):
-            nx = dx[i] + x
-            ny = dy[i] + y
-            if 0<= nx <n and 0<= ny <m and visited[nx][ny] == 0 and paint[nx][ny] == 1:
-                visited[nx][ny] = 1
-                q.append((nx,ny))
-                cnt+=1
-    return cnt
+    for i in range(4):
+      nx = x + dx[i]
+      ny = y + dy[i]
+      if 0 <= nx < n and 0 <= ny < m :
+        if visited[nx][ny] == 0 and board[nx][ny] == 1:
+          q.append((nx, ny))
+          visited[nx][ny] = 1
+          size += 1
+  return size 
+    
 
-ans = 0
-ans1 = 0
+n, m = map(int, input().split(" "))
+board = [list(map(int, input().split(" "))) for _ in range(n)]
+visited = [[0] * m for _ in range(n)]
+
+cnt = 0
+maxsize = 0
 for i in range(n):
-    for j in range(m):
-        if paint[i][j] == 1 and visited[i][j] == 0:
-            ans1+=1
-            ans = max(ans, bfs(i,j))
+  for k in range(m):
+    if visited[i][k] == 0 and board[i][k] == 1:
+      size = bfs(i, k, visited)
+      if size > maxsize :
+        maxsize = size
+      cnt += 1
 
-print(ans1)
-print(ans)
+print(cnt)
+print(maxsize)
