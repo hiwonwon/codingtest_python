94dfd8c9001a219c5bbe0d41f45d7522e693e1c9
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2024-07-10 14:30:05 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 48 ms, Memory: 40556 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -1,23 +1,18 @@
-n = int(input())
-ans = [['*']*n for _ in range(n)]
-# n이 3의 몇승인지 구함
-k = 0
-tmp = n
-while(tmp > 1 ):
-    tmp /=3
-    k += 1
+def draw_stars(n):
+  if n==1:
+    return ['*']
 
-for l in range(1,k+1):
-    sqt = 3**l
-    for i in range(n):
-        for j in range(n):
-            if i % sqt == 0 and j % sqt == 0:
-                for x in range(0,sqt//3):
-                    for y in range(0,sqt//3):
-                        if i+x+sqt//3<n and j+y+sqt//3<n:
-                            ans[i+x+sqt//3][j+y+sqt//3] = ' '
+  Stars=draw_stars(n//3)
+  L=[]
 
-for i in range(n):
-    for j in range(n):
-        print(ans[i][j], end="")
-    print()
+  for star in Stars:
+    L.append(star*3)
+  for star in Stars:
+    L.append(star+' '*(n//3)+star)
+  for star in Stars:
+    L.append(star*3)
+
+  return L
+
+N=int(input())
+print('\n'.join(draw_stars(N)))
