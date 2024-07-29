d36539c6c48c0c82aeaedd6f0782c7b0f40e591e
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2024-07-29 23:14:12 +0900

    [Gold V] Title: 리모컨, Time: 608 ms, Memory: 110604 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.py
+++ 백준/Gold/1107. 리모컨/리모컨.py
@@ -1,25 +1,24 @@
-def is_channel_reachable(channel,broken_buttons):
-    channel_str=str(channel)
-    for char in channel_str:
-        if int(char) in broken_buttons:
-            return False
-    return True
+n = int(input())
+m = int(input())
+lst =[]
+if m >0:
+    lst = list(input().split())
 
-def minimum_button_presses(N,broken_buttons):
-    current_channel=100
-    min_presses=abs(N-current_channel)
-    for channel in range(max(0,N-500000),N+500000):
-        if is_channel_reachable(channel,broken_buttons):
-            presses=len(str(channel))+abs(N-channel)
-            min_presses=min(min_presses, presses)
-    return min_presses
 
-N=int(input())
-M=int(input())
-if M > 0:
-    broken_buttons=set(map(int, input().split()))
+now = 100
+answer = abs(n-now)
+if n== now:
+    print(0)
 else:
-    broken_buttons=set()
-
-result=minimum_button_presses(N,broken_buttons)
-print(result)
+    last = '9'*(len(str(n))+1)
+    for i in range(int(last)):
+        flag = True
+        num = str(i)
+        for k in lst:
+            if k in num:
+                flag = False
+                break
+        if flag:
+            tmp = len(num) + abs(n-i)
+            answer = min(tmp, answer)
+    print(answer)
