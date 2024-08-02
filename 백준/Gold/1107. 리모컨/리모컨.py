d0beea04b9cc99ef21c85f41717448b141c4d2d7
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2024-08-02 13:51:40 +0900

    [Gold V] Title: 리모컨, Time: 1204 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.py
+++ 백준/Gold/1107. 리모컨/리모컨.py
@@ -1,24 +1,26 @@
-n = int(input())
-m = int(input())
-lst =[]
-if m >0:
-    lst = list(input().split())
+def is_possible(channel, broken_buttons):
+    for ch in str(channel):
+        if int(ch) in broken_buttons:
+            return False
+    return True
 
+def min_button_presses(target_channel, broken_buttons):
+    min_presses = abs(target_channel - 100)  # +, - 버튼만으로 이동하는 경우
 
-now = 100
-answer = abs(n-now)
-if n== now:
-    print(0)
+    for channel in range(1000000):  # 채널 범위 설정
+        if is_possible(channel, broken_buttons):
+            presses = len(str(channel)) + abs(channel - target_channel)
+            min_presses = min(min_presses, presses)
+
+    return min_presses
+
+# 입력 받기
+target_channel = int(input())
+broken_count = int(input())
+if broken_count > 0:
+    broken_buttons = set(map(int, input().split()))
 else:
-    last = '9'*(len(str(n))+1)
-    for i in range(int(last)):
-        flag = True
-        num = str(i)
-        for k in lst:
-            if k in num:
-                flag = False
-                break
-        if flag:
-            tmp = len(num) + abs(n-i)
-            answer = min(tmp, answer)
-    print(answer)
+    broken_buttons = set()
+
+# 결과 출력
+print(min_button_presses(target_channel, broken_buttons))
