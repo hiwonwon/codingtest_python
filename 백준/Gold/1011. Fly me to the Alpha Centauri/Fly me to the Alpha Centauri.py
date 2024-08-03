191ec3087df7bd6f40fd2325b311217b8cda8166
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2024-08-03 21:42:24 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 40 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.py
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.py
@@ -1,20 +1,18 @@
+from math import sqrt
 
-
-
-T = int(input())
-for i in range(T):
-    x,y = map (int,input().split())
-    d = y-x
-    # 이동거리
-    tmp = 0
-    #이동횟수
-    cnt = 0
-    #반복 횟수
-    moving = 0
-    i = 0
-    while ( tmp < d):
-        cnt += 1
-        if cnt%2 != 0:
-            moving += 1
-        tmp+= moving
-    print(cnt)
+TC = int(input())
+for T in range(1, TC + 1):
+    x, y = map(int, input().split())
+    l = y - x
+    n = int(sqrt(l))
+    # 각 제곱에 매칭되는 홀수
+    m = n * 2 - 1
+    # n^2과 (n+1)^2의 반절보다 작다면..
+    if l == n * n:
+        print(m)
+    elif n * n < l <= n * n + n:
+        print(m + 1)
+    elif n * n + n < l < n * n + 2 * n + 1:
+        print(m + 2)
+    elif l == (n + 1) * (n + 1):
+        print(m + 2)
