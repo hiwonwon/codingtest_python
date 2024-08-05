287e846aa404504f834cbbc034f9bb204b496824
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2024-08-05 23:34:06 +0900

    [Gold V] Title: 동전 1, Time: 168 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.py
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -1,9 +1,11 @@
+import sys
+input = sys.stdin.readline
 n, k = map(int, input().split())
 coins = [int(input()) for _ in range(n)]
 dp = [0] * (k+1)
 dp[0] = 1
-for c in coins:
-    for j in range(c,k+1):
-        dp[j] = dp[j] + dp[j-c]
 
+for coin in coins:
+    for i in range(coin, k+1):
+        dp[i] += dp[i-coin]
 print(dp[k])
