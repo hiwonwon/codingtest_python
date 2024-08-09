783448296bcdb6a981db0bd64f69e209af4f091b
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2024-08-09 23:39:24 +0900

    [Gold V] Title: 로봇 청소기, Time: 36 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -1,85 +1,40 @@
-n, m = map(int, input().split())
-start = list(map(int, input().split()))
-rooms = [list(map(int, input().split())) for _ in range(n)]
+import sys
 
-from collections import deque
-visited = [[0]*m for _ in range(n)]
+N, M = map(int, sys.stdin.readline().split()) # 방의 크기
+r, c, d = map(int, sys.stdin.readline().split()) # 시작 좌표, 방향
+# 0 북 / 1 동 / 2 남 / 3 서
 
+arr = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 
-ans = 0
-x,y,d = start[0] , start[1], start[2]
-q = deque()
-q.append((x,y,d))
+di = [-1, 0, 1, 0] # 북 동 남 서
+dj = [0, 1, 0, -1]
+k = d
+i, j = r, c 
+cnt = 0
 
-visited[x][y] = 1
-#시작하는 곳이 청소가 안된 빈칸일 때
-if rooms[x][y] == 0:
-    ans += 1
-    #청소된 곳은 2로 표시
-    rooms[x][y] = 2
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
+while 1:
+    # print(i, j)
+    # print(cnt)
+    # print(arr)
+    if arr[i][j] == 0:  # 청소하기
+        cnt += 1
+        arr[i][j] = 3 
+    
+    if 0 < i < N - 1 and 0 < j < M - 1 and (0 in [arr[i - 1][j], arr[i][j + 1], arr[i + 1][j], arr[i][j - 1]]):
+         # 주변에 청소하지 않은 칸이 있다면        
+        while 1:
+            k = (k + 3) % 4  # 90도 회전
+            if arr[i + di[k]][j + dj[k]] == 0:
+                i += di[k]
+                j += dj[k] 
+                break # 90도 회전 멈춤
 
-while(q):
-    x,y,d = q.popleft()
+    else: # 주변이 다 청소됐다면
+        if 0 <= i + (di[k] * -1) < N and  0 <= j + (dj[k] * -1) < M and arr[i + (di[k] * -1)][j + (dj[k] * -1)] != 1:   
+            # 방향을 유지한 채로 뒤가 벽이 아니라면
+            i += (di[k] * -1)     # 후진
+            j += (dj[k] * -1) 
+        else:   # 뒤가 벽이라면
+            break
 
-    #주변 4칸 확인
-    check = False
-    for i in range(4):
-        nx = dx[i] + x
-        ny = dy[i] + y
-        if 0<= nx <n and 0<= ny <m and rooms[nx][ny] == 0:
-            check =True
-
-    #주변 4칸 중 청소되지 않은 칸이 있는 경우
-    if check:
-        #반시계 방향으로 90도 회전
-        if d == 0:
-            nd = 3
-        else:
-            nd = d-1
-
-        # 앞쪽 칸으로 전진후 좌표 구하기
-        if nd == 0:
-            nx = x -1
-            ny = y
-        elif nd ==1:
-            nx = x
-            ny = y+1
-        elif nd == 2:
-            nx = x+1
-            ny = y
-        elif nd == 3:
-            nx = x
-            ny = y-1
-        #전진할 좌표가 청소되지 않은 빈칸인경우에 전진
-        if 0<= nx <n and 0<=  ny < m  and rooms[nx][ny]==0:
-            q.append((nx,ny,nd))
-            ans += 1
-            #청소된 곳은 2로 표시
-            rooms[nx][ny] = 2
-        #청소되지않은 빈칸이 주변에 있지만 현재의 반시계방향으로 전진했을 때 빈칸이 아닌 경우
-        else:
-            q.append((x,y,nd))
-    #주변4칸중 청소되지않은 빈칸 없을 경우
-    else:
-        if d == 0:
-            nx = x+1
-            ny = y
-        elif d ==1:
-            nx = x
-            ny = y-1
-        elif d == 2:
-            nx = x -1
-            ny = y
-        elif d == 3:
-            nx = x
-            ny = y+1
-        if 0<= nx <n and 0<=  ny < m  and rooms[nx][ny]!=1:
-            q.append((nx,ny,d))
-        if rooms[nx][ny]==1:
-            print(ans)
-            exit()
-print(ans)                
-
-
+print(cnt)
