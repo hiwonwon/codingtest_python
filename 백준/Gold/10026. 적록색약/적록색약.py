bc1f019a264ab595a7661353b88d709cb7d23634
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2024-08-12 20:40:33 +0900

    [Gold V] Title: 적록색약, Time: 68 ms, Memory: 31564 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,34 +1,39 @@
 import sys
-sys.setrecursionlimit(20000)
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
+n = int(input())
+graph = []
+visited=[[0]*n for _ in range(n)]
+cnt, cnt1 = 0,0
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
 
-N = int(input())
-visited = [[1]*N for _ in range(N)]
-visited_redgreen = [[1]*N for _ in range(N)]
-graph = [list(input()) for _ in range(N)]
-graph_redgreen = [[1]*N for _ in range(N)]
-for j in range(N):
-    for k in range(N):
-        graph_redgreen[j][k] = graph[j][k]
-        if graph[j][k] == 'G':
-            graph_redgreen[j][k] = 'R'
-# print(graph,graph_redgreen)
-def dfs(x,y,color,visited,graph):
-    visited[x][y]= 0
-    for a,b in [[0,1],[0,-1],[1,0],[-1,0]]:
-        if 0 <= a+x < N and 0 <= b+y < N:
-            if visited[a+x][b+y] and graph[a+x][b+y] == color:
-                dfs(a+x,b+y,color,visited,graph)
+for i in range(n):
+    graph.append(list(input().strip()))
 
-cnt_mid = 0
-cnt_redgren = 0
-for i in range(N):
-    for j in range(N):
-        if visited[i][j]:
-            cnt_mid += 1
-            dfs(i,j,graph[i][j],visited,graph)
-        if visited_redgreen[i][j]:
-            cnt_redgren += 1
-            dfs(i,j,graph_redgreen[i][j],visited_redgreen,graph_redgreen)
+def dfs(x,y):
+    visited[x][y] = 1
+    
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0<=nx<n and 0<=ny<n and not visited[nx][ny]:
+            if graph[nx][ny] == graph[x][y]:
+                dfs(nx,ny)
 
-print(cnt_mid, cnt_redgren)
+for i in range(n):
+    for j in range(n):
+        if not visited[i][j]:
+            dfs(i,j)
+            cnt += 1
+for i in range(n):
+    for j in range(n):
+        if  graph[i][j] == "G":
+            graph[i][j] = "R"
+visited = [[0]*n for _ in range(n)]
+for i in range(n):
+    for j in range(n):
+        if not visited[i][j]:
+            dfs(i,j)
+            cnt1 += 1
+print(cnt, cnt1)
