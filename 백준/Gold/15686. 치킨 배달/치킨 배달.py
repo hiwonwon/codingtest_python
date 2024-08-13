77bb248d2ec1a7da15bf6b01664ff77b80f8910b
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2024-08-13 23:11:34 +0900

    [Gold V] Title: 치킨 배달, Time: 460 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,31 +1,47 @@
-n, m = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
+import sys
 
-home = []
+n,m = map(int,input().split())
+board = [list(map(int,input().split())) for _ in range(n)]
+
 chicken = []
+house = []
 
-#치킨집과 집이있는 좌표의 값을 따로 리스트에 받아줌
 for i in range(n):
     for j in range(n):
-        if graph[i][j] == 1:
-            home.append([i+1,j+1])
-        if graph[i][j] == 2:
-            chicken.append([i+1,j+1])
-from itertools import combinations
+        if board[i][j] == 2:
+            chicken.append([i,j])
+        elif board[i][j] == 1:
+            house.append([i,j])
 
-ans = float('inf')
-for c in combinations(chicken,m):
-    #m개의 치킨집 고르는 것 반복
-    temp = 0
+target = []
+chick_len = len(chicken)
+arr = [[0,0] for _ in range(m)]
+visited = [0]*m
 
-    for h in home:
-        #각 집에 가장 가까운 치킨집의 치킨거리 구함
-        cd = float('inf')
-        for j in range(m):
-            cd = min(cd,abs(h[0]-c[j][0])+abs(h[1]-c[j][1]))
-        #해당 집에서 가장 가까운 치킨집의 치킨거리 도시의 치킨거리에 구해줌
-        temp += cd
-    #구한 도시의 치킨거리중 최솟값 구함
-    ans = min(ans,temp)
 
-print(ans)
+min_chicken_dis = sys.maxsize
+
+
+def dfs(start, cnt):
+    global min_chicken_dis
+    global chicken_dis
+    if cnt==m:
+        city_chicken = 0
+        for h in range(len(house)):
+            chicken_dis = sys.maxsize
+            for i in range(m):
+                dis = abs(arr[i][0]-house[h][0]) + abs(arr[i][1]-house[h][1])
+                chicken_dis = min(chicken_dis,dis)
+            city_chicken += chicken_dis
+        min_chicken_dis = min(min_chicken_dis,city_chicken)
+        return
+
+    for i in range(start,chick_len):
+        arr[cnt][0],arr[cnt][1] = chicken[i][0],chicken[i][1]
+        dfs(i+1, cnt+1)
+
+dfs(0,0)
+
+print(min_chicken_dis)
+
+
