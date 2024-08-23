a0871e98d612565f8d595f9b2274f8992fa5650d
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2024-08-23 14:11:00 +0900

    [Gold V] Title: 치킨 배달, Time: 160 ms, Memory: 111728 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,47 +1,28 @@
 import sys
+from itertools import combinations
 
-n,m = map(int,input().split())
-board = [list(map(int,input().split())) for _ in range(n)]
+input = sys.stdin.readline
 
+N, M = map(int, input().split())
 chicken = []
 house = []
 
-for i in range(n):
-    for j in range(n):
-        if board[i][j] == 2:
-            chicken.append([i,j])
-        elif board[i][j] == 1:
-            house.append([i,j])
+for i in range(N):
+    row = list(map(int, input().split()))
+    for j in range(N):
+        if row[j] == 1:
+            house.append((i, j))
+        elif row[j] == 2:
+            chicken.append((i, j))
 
-target = []
-chick_len = len(chicken)
-arr = [[0,0] for _ in range(m)]
-visited = [0]*m
+def get_distance(a, b):
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
 
+def get_city_chicken_distance(selected_chicken):
+    return sum(min(get_distance(h, c) for c in selected_chicken) for h in house)
 
-min_chicken_dis = sys.maxsize
+min_distance = float('inf')
+for combi in combinations(chicken, M):
+    min_distance = min(min_distance, get_city_chicken_distance(combi))
 
-
-def dfs(start, cnt):
-    global min_chicken_dis
-    global chicken_dis
-    if cnt==m:
-        city_chicken = 0
-        for h in range(len(house)):
-            chicken_dis = sys.maxsize
-            for i in range(m):
-                dis = abs(arr[i][0]-house[h][0]) + abs(arr[i][1]-house[h][1])
-                chicken_dis = min(chicken_dis,dis)
-            city_chicken += chicken_dis
-        min_chicken_dis = min(min_chicken_dis,city_chicken)
-        return
-
-    for i in range(start,chick_len):
-        arr[cnt][0],arr[cnt][1] = chicken[i][0],chicken[i][1]
-        dfs(i+1, cnt+1)
-
-dfs(0,0)
-
-print(min_chicken_dis)
-
-
+print(min_distance)
