a5b8857e9813e2c26065cec29c7bb8baa490b686
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2024-08-26 23:19:07 +0900

    [Silver I] Title: 곱셈, Time: 80 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -1,13 +1,2 @@
-a,b,c = map(int, input().split())
-
-def ans(a,b,c):
-    if b == 1:
-        return a%c
-    else:
-        k = ans(a,b//2,c)
-        if b%2 == 0 :
-            return(k*k%c)
-        else:
-            return(k*k*a%c)
-
-print(ans(a,b,c))
+a,b,c=map(int,input().split())
+print(pow(a,b,c))
