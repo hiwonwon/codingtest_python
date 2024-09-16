28561f12a2d128d779a0fa3fc2e450f694a78dfd
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2024-09-16 21:05:22 +0900

    [Gold V] Title: 동전 1, Time: 92 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.py
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -1,11 +1,18 @@
-n, k = map(int, input().split())
+n,k=map(int,input().split())
+lst=[]
+for i in range(n):
+    a=int(input())
+    lst.append(a)
 
-D = [1] + [0] * (k)
+def coin_combinations(coins, target):
+    dp = [0] * (target + 1)  # 목표 금액만큼 DP 테이블 생성
+    dp[0] = 1  # 0원을 만드는 방법은 1가지 (아무 동전도 사용하지 않는 경우)
 
-for _ in range(n):
-    i = int(input())
-    for j in range(k):
-        if D[j] != 0 and j + i <= k:
-            D[j + i] += D[j]
+    for coin in coins:  # 각 동전별로 DP 테이블 갱신
+        for j in range(coin, target + 1):  # 동전 금액부터 목표 금액까지 확인
+            dp[j] += dp[j - coin]  # dp[j - coin]의 값을 더함
+    
+    return dp[target]
 
-print(D[-1])
+
+print(coin_combinations(lst,k))
