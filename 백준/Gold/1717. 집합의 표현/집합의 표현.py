5b0851adca762ba5da98e40826e04e9958b65c15
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2024-09-18 23:41:33 +0900

    [Gold V] Title: 집합의 표현, Time: 228 ms, Memory: 76584 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.py
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -1,38 +1,39 @@
 import sys
+
+sys.setrecursionlimit(10**7)
+def find_parent(a):
+    global parent_list
+
+    if parent_list[a] == a:
+        return a
+
+    parent_list[a] = find_parent(parent_list[a])
+    return parent_list[a]
+
+def union(a, b):
+    global parent_list
+    parent_a = find_parent(a)
+    parent_b = find_parent(b)
+
+    if parent_a < parent_b:
+        parent_list[parent_b] = parent_a
+    else:
+        parent_list[parent_a] = parent_b
+        
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
-parent = [i for i in range(n + 1)]
-rank = [0] * (n + 1)
-
-
-def find(v):
-    if parent[v] == v:
-        return v
-    else:
-        parent[v] = find(parent[v])
-        return parent[v]
-
-
-def union(a, b):
-    a = find(a)
-    b = find(b)
-    if a == b:
-        return False
-
-    if rank[a] < rank[b]:
-        a, b = b, a
-    parent[b] = a
-
-    if rank[a] == rank[b]:
-        rank[a] += 1
-    return True
-
+parent_list = [i for i in range(n+1)]
 
 for _ in range(m):
-    q, a, b = map(int, input().split())
-    if q == 0:
+    operand, a, b = map(int, input().split())
+
+    if operand == 0:
         union(a, b)
     else:
-        print('YES' if find(a) == find(b) else 'NO')
+        if find_parent(a) == find_parent(b):
+            sys.stdout.write("YES\n")
+        else:
+            sys.stdout.write("NO\n")
+
