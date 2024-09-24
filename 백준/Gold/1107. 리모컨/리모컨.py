b524643ed08e554069bde4a737a9acaf0bd447c4
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2024-09-24 23:52:44 +0900

    [Gold V] Title: 리모컨, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.py
+++ 백준/Gold/1107. 리모컨/리모컨.py
@@ -1,38 +1,69 @@
 n = int(input())
 m = int(input())
-if m == 0:
-    print(min(abs(n-100), len(str(n))))
-else:
-    tmp = list(map(int, input().split()))
-    l = []
-    for i in range(10):
-        if i not in tmp:
-            l.append(i)
-    if len(l)==1:
-        length = len(str(n))
-        m = abs(100-n)
-        for i in range(1,length+2):
-            m = min(m, abs(n-l[0]*((10**i-1)//9))+i)
-        print(m)
-    elif len(l)==0:
-        print(abs(n-100))
-    else:
-        length = len(str(n))
-        cand = []
-        if length != 1:
-            cand.append(abs(n-l[-1]*((10**(length-1)-1)//9))+length-1)
-        def cal(x,t):
-            if t == 0:
-                cand.append(abs(n-x)+length)
-                return
-            for i in l:
-                cal(10*x+i, t-1)
-            return
-        cal(0, length)
-        if l[0]==0:
-            cand.append(abs(n-l[1]*(10**length))+length+1)
+button = [i for i in range(10)]
+if m > 0:
+    broken = list(map(int, input().split(' ')))
+    for b in broken:
+        button.remove(b)
+
+def valid(n):
+    while n > 0:
+        if (n % 10) not in button:
+            return False
+        n = n // 10
+    return True
+
+# 주어진 숫자로 만들 수 있는 n에 가장 가까운 채널
+def nearest(n):
+    if n < min(button):
+        return min(button) - n + 1
+    digit = []
+    k = n
+    while k > 0:
+        digit.append(k % 10)
+        k = k // 10
+    low, high = (0, 0)
+    
+    # low 구하기
+    i = len(digit) - 1
+    while i >= 0:
+        low = low * 10 + digit[i]
+        if digit[i] in button:
+            i -= 1
+            continue
         else:
-            t = l[0]
-            cand.append(abs(n-t*((10**(length+1)-1)//9))+length+1)
-        cand.append(abs(n-100))
-        print(min(cand))
+            while low > 0:
+                low -= 1
+                if (low % 10) in button:
+                    break
+            for j in range(i):
+                low = low * 10 + max(button)
+            break
+    
+    # high 구하기
+    i = len(digit) - 1
+    while i >= 0:
+        high = high * 10 + digit[i]
+        if digit[i] in button:
+            i -= 1
+            continue
+        else:
+            while True:
+                high += 1
+                if (high % 10) in button:
+                    break
+            for j in range(i):
+                high = high * 10 + min(button)
+            break
+    
+    d = []
+    if valid(low):
+        d.append((n - low) + len(str(low)))
+    if valid(high):
+        d.append((high - n) + len(str(high)))
+    return min(d)
+                  
+if button:
+    print(min(nearest(n), abs(n - 100)))
+else:
+    print(abs(n - 100))
