6d1ac2a649bfccbed5ca0c3bcf92f8781a149ec7
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2024-09-26 16:01:31 +0900

    [Gold V] Title: 집합의 표현, Time: 216 ms, Memory: 64564 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.py
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -1,39 +1,22 @@
 import sys
-
-sys.setrecursionlimit(10**7)
-def find_parent(a):
-    global parent_list
-
-    if parent_list[a] == a:
-        return a
-
-    parent_list[a] = find_parent(parent_list[a])
-    return parent_list[a]
-
-def union(a, b):
-    global parent_list
-    parent_a = find_parent(a)
-    parent_b = find_parent(b)
-
-    if parent_a < parent_b:
-        parent_list[parent_b] = parent_a
-    else:
-        parent_list[parent_a] = parent_b
-        
-input = sys.stdin.readline
-
-n, m = map(int, input().split())
-
-parent_list = [i for i in range(n+1)]
-
+n,m = map(int, input().split())
+s = [-1]*(n+1)
+ipt = list(map(int, sys.stdin.read().split()))
+def find(x):
+    if s[x] == -1:
+        return x
+    s[x] = find(s[x])
+    return s[x]
+def plus(x,y):
+    if find(x)!=find(y):
+        s[find(y)] = find(x)
+    return
 for _ in range(m):
-    operand, a, b = map(int, input().split())
-
-    if operand == 0:
-        union(a, b)
+    cal, a, b = ipt[_*3], ipt[_*3+1], ipt[_*3+2]
+    if cal == 0:
+        plus(a,b)
     else:
-        if find_parent(a) == find_parent(b):
-            sys.stdout.write("YES\n")
+        if find(a) == find(b):
+            print("YES")
         else:
-            sys.stdout.write("NO\n")
-
+            print("NO")
