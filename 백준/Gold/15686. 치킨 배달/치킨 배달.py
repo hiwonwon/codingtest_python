aac6e4ba9ca8b58e6fbf8672092e7e89a020b795
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2024-10-01 19:25:09 +0900

    [Gold V] Title: 치킨 배달, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,46 +1,47 @@
-# from heapq import heappush, heappop, heapify
-# from collections import deque
-from itertools import combinations
-# from bisect import bisect_left, bisect_right
 import sys
 input = sys.stdin.readline
-mis = lambda: map(int, input().split())
-INF = float('inf')
-# mOD = 10**9 + 7
-# sys.setrecursionlimit(10**6)
-# dx = [0, 1, -1, 0]
-# dy = [1, 0, 0, -1]
-# dx = [0, -1, 1, 0, -1, -1, 1, 1]
-# dy = [1, 0, 0, -1, 1, -1, 1, -1]
-
-n, m = mis()
-city = [[*mis()] for _ in range(n)]
-
-houses = []
-chickens = []
-for i in range(n):
-    for j in range(n):
-        if city[i][j] == 1:
-            houses.append((i, j))
-        elif city[i][j] == 2:
-            chickens.append((i, j))
-
-def solve(arr):
+INF = 10000000
+def dfs(dep, n):
+    global ans
     ret = 0
-    for i in houses:
-        dist = INF
-        
-        for j in arr:
-            tmp = abs(i[0] - j[0]) + abs(i[1] - j[1])
-            dist = min(dist, tmp)
-            
-        ret += dist
-        
-    return ret
+    if dep == M:
+        for d in dist:
+            for distence, idx in d:
+                if visited[idx]:
+                    ret += distence
+                    break
+            if ans <= ret:
+                return
+        ans = min(ans, ret)
+        return
+	
+    for i in range(n, len(chicken)):
+        if not visited[i]:
+            visited[i] = 1
+            dfs(dep+1, i+1)
+            visited[i] = 0
+    return
 
-ans = INF
-for i in combinations(chickens, m):
-    distance = solve(i)
-    ans = min(ans, distance)
-
-print(ans)
+if __name__ == '__main__':
+    N, M = map(int, input().split())
+    city =[list(map(int, input().split())) for _ in range(N)]
+    house = []
+    chicken = []
+    for i in range(N):
+        for j in range(N):
+            if city[i][j] == 1:
+                house.append((i, j))
+            elif city[i][j] == 2:
+                chicken.append((i, j))
+    dist = []
+    for r1, c1 in house:
+        dist.append([])
+        for idx, c in enumerate(chicken):
+            r2, c2 = c
+            dist[-1].append((abs(r1-r2)+abs(c1-c2), idx))
+        dist[-1].sort()
+    
+    visited = [0 for _ in range(len(chicken))]
+    ans = INF
+    dfs(0, 0)
+    print(ans)
