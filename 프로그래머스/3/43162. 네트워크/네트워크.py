a92148d72fd00d7ab35764ebfe1310eeeaf99269
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2024-10-04 21:41:37 +0900

    [level 3] Title: 네트워크, Time: 0.67 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,24 +1,19 @@
-
 def solution(n, computers):
     answer = 0
-    visited = [False]*n
+    visited = [False for _ in range(n)]
     
-    def dfs(i):
-        visited[i] = True
-        
-        for j in range(n):
-            #아직 방문안한 컴퓨터이며 i번째 컴퓨터와 연결된 컴퓨터 방문
-            if visited[j]==False and i != j and computers[i][j] == 1:
-                visited[j] = True
-                dfs(j)
-                
-    
-    for i in range(n):
-        if visited[i] == False:
-            dfs(i)
+    for c in range(n):
+        if visited[c] == False:
+            dfs(n,computers,c,visited)
             answer+=1
-        
-            
-            
-
     return answer
+
+def dfs(n,computers,c,visited):
+    visited[c] = True
+    for i in range(n):
+        #본인이 아닌 컴퓨터와 연결되어 있다면
+        if i != c and computers[c][i] == 1:
+            #그리고 방문한적 없는 컴퓨터라면
+            if visited[i] == False:
+                dfs(n,computers,i,visited)
+            
