5f5dd2f874bcad0266664794101172d33613c113
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2024-10-09 22:34:14 +0900

    [Gold V] Title: 탑, Time: 496 ms, Memory: 144816 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -1,15 +1,16 @@
 n = int(input())
-arr = list(map(int, input().split()))
-ans = [0 for _ in range(n)]
+tower = list(map(int, input().split()))
 
-top = [(1, arr[0])]
-for i in range(1, n):
-    while top and top[-1][1] < arr[i]:
-        top.pop()
-    if top:     # 더 큰 탑이 존재한다면
-        ans[i] = top[-1][0]
-    else:       # 현재 제일 큰 탑이라면
-        ans[i] = 0
-    top.append((i+1, arr[i]))
+result = [0] * n
+stack = []
+
+for i in range(n):
+    while stack and stack[-1][0] < tower[i]:
+        stack.pop()
     
-print(*ans)
+    if stack:
+        result[i] = stack[-1][1]
+    
+    stack.append((tower[i], i+1))
+        
+print(' '.join(map(str, result)))
