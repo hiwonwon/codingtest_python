599bd6d3b5a7eb88f87711b0e7674b92740246b2
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2024-10-11 23:41:01 +0900

    [Gold V] Title: 동전 2, Time: 164 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.py
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -1,21 +1,15 @@
-n, k =map(int, input().split())
-coins = [int(input()) for _ in range(n)]
-#중복 제거
-coins = list(set(coins))
-coins.sort()
-inf = float('inf')
-dp = [inf] * (k+1)
+n, k = map(int, input().split())
+
+coins = []
+for _ in range(n):
+    coins.append(int(input()))
+
+dp = [float('inf')] * (k + 1)
 dp[0] = 0
 
-for c in coins:
-    #현재 동전보다 작은 값은 갱신되지 않으므로 현재 동전의 값부터 시작
-    for i in range(c,k+1):
-        dp[i] = min(dp[i],dp[i-c]+1)
+for coin in coins:
+    for i in range(coin, k + 1):
+        if dp[i] > (dp[i - coin] + 1):
+            dp[i] = dp[i - coin] + 1
 
-
-
-#불가능한 경우
-if dp[k]>10000:
-    print(-1)
-else:
-    print(dp[k])
+print(dp[k] if dp[k] < float('inf') else -1)
