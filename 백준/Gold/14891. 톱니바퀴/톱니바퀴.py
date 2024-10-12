65f8a09c4981ff32fcf44fc325f4db415f138a90
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2024-10-12 22:58:07 +0900

    [Gold V] Title: 톱니바퀴, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
@@ -1,65 +1,39 @@
-def sigye(toapn):
-    last= toapn[-1]
-    toapn[1:] = toapn[0:7]
-    toapn[0] = last
-    return toapn
-
-def bansigye(toapn):
-    first= toapn[0]
-    toapn[0:7] = toapn[1:]
-    toapn[-1] = first
-    return toapn
-
-toap = []
-for i in range(4):
-    toap.append(list(input()))
-
-k = int(input())
+# https://www.acmicpc.net/problem/14891
 
 
-for _ in range(k):
-    n,s = map(int, input().split())
-    scheck =[0] *4
-    #회전한 톱니바퀴 인덱스에 맞추어 마이너스
-    n -= 1
-    scheck[n] = s
+class Gear:
+    def __init__(self, status):
+        self._status = status
 
-    #n번째 톱 이전의 톱 돌리기
-    for i in range(n-1,-1,-1):
-        #맞닿은 극이 반대일 때
-        if toap[i+1][6] != toap[i][2]:
-            if scheck[i+1] == 1:
-                scheck[i] = -1
-            if scheck[i+1] == -1:
-                scheck[i] = 1
+    def status(self):
+        return self._status
+
+    def rotate(self, rdir):
+        if rdir == 1:
+            self._status = self._status[-1] + self._status[:-1]
         else:
-            #회전이 더이상 없으므로 브레이크
-            break
-    #n번째 톱 이후의 톱 돌리기
-    for i in range(n+1,4):
-        #맞닿은 극이 반대일 때
-        if toap[i][6] != toap[i-1][2]:
-            if scheck[i-1] == 1:
-                scheck[i] = -1
-            if scheck[i-1] == -1:
-                scheck[i] = 1
+            self._status = self._status[1:] + self._status[0]
 
-        else:
-            #회전이 더이상 없으므로 브레이크
-            break
-    #이번 회전에서의 극의 값에 따른 회전여부 저장해서 한번에 회전
-    for i in range(4):
-        if scheck[i] == 1:
-            toap[i] = sigye(toap[i])
-        if scheck[i]== -1:
-            toap[i] = bansigye(toap[i])
-    
+    def left(self):
+        return self._status[-2]
 
-ans = 0
+    def right(self):
+        return self._status[2]
 
 
-for i in range(4):
-    if toap[i][0] == "1":
-        ans += pow(2,i)
-#print(toap)
-print(ans)
+G = [Gear(input()) for _ in range(4)]
+
+
+def rotate(n, dir, rdir):
+    if dir >= 0 and n != 3 and G[n].right() != G[n + 1].left():
+        rotate(n + 1, 1, -rdir)
+    if dir <= 0 and n != 0 and G[n].left() != G[n - 1].right():
+        rotate(n - 1, -1, -rdir)
+    G[n].rotate(rdir)
+
+
+for _ in range(int(input())):
+    n, rdir = map(int, input().split())
+    rotate(n - 1, 0, rdir)
+
+print(sum([2 ** (i) for i in range(4) if G[i].status()[0] == "1"]))
