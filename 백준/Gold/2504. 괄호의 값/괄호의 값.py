ae227716cc0e66fe2778649b2b5ff33ca349ddc4
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2024-10-20 21:02:28 +0900

    [Gold V] Title: 괄호의 값, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2504. 괄호의 값/괄호의 값.py
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.py
@@ -1,66 +1,54 @@
-def para_check(st):
-    para_s = 0
-    para_b = 0
-    last = []
-    right = 1
-    for i in range(len(st)):
-        if st[i] == '(':
-            para_s += 1
-            last.append('(')
-        elif st[i] == '[':
-            para_b += 1
-            last.append('[')
-        else:
-            if st[i]==')':
-                if para_s <= 0 or last[-1] != '(':
-                    right = 0
-                else:
-                    para_s -= 1
-                    last.pop(-1)
-            if st[i]==']':
-                if para_b <= 0 or last[-1] != '[':
-                    right = 0
-                else:
-                    para_b -= 1
-                    last.pop(-1)
-    if para_s != 0 or para_b != 0:
-        right = 0
-    return right
+#2504
 
-def para_count(st):
-    b = 1
-    for j in range(1, len(st)):
-        if para_check(st[0:j]) == 1 and para_check(st[j:len(st)]) == 1:
-            b = 0
-            return 1 + para_count(st[j+1:len(st)])
-    if b == 1:
-        return b
-def para_change(st):
-    #print(st)
-    if len(st) == 0:
+def bracket_value(bracket_str):
+    result = 0
+    stack = []
+    substr = ""
+
+    if len(bracket_str) == 0:
         return 1
-    elif st == '()':
-        return 2
-    elif st == '[]':
-        return 3
-    if para_count(st) != 1:
-        i = 0
-        s = 0
-        while i < len(st):
-            for j in range(i+1, len(st)):
-                if para_check(st[i:j+1]) == 1 and (i != 0 or j != len(st)-1):
-                    s += para_change(st[i:j+1])
-                    i = j + 1
-                    break
-        return s
+    
+    for i in range(len(bracket_str)):
+        if bracket_str[i] == '(' or bracket_str[i] == '[':
+            if len(stack) != 0:
+                substr = substr + bracket_str[i]
+            stack.append(bracket_str[i])
+        elif bracket_str[i] == ')':
+            if len(stack) > 0:
+                pop = stack.pop()
+                if pop == '(' and len(stack) == 0:
+                    value = bracket_value(substr)
+                    if value == 0:
+                        return 0
+                    result += 2 * value
+                    substr = ""
+                elif pop == '(' and len(stack) != 0:
+                    substr = substr + bracket_str[i]
+                else:
+                    return 0
+            else:
+                return 0
+        else: # bracket_str[i] == ']'
+            if len(stack) > 0:
+                pop = stack.pop()
+                if pop == '[' and len(stack) == 0:
+                    value = bracket_value(substr)
+                    if value == 0:
+                        return 0
+                    result += 3 * value
+                    substr = ""
+                elif pop == '[' and len(stack) != 0:
+                    substr = substr + bracket_str[i]
+                else:
+                    return 0
+            else:
+                return 0
+        
+    if len(stack) != 0:
+        return 0
     else:
-        if st[0] == '(':
-            return 2 * para_change(st[1:-1])
-        elif st[0] == '[':
-            return 3 * para_change(st[1:-1])
-st = input()
-if para_check(st) == 0:
-    print(0)
-else:
-    print(para_change(st))
-            
+        return result
+    
+bracket_str = input()
+
+print(bracket_value(bracket_str))
