47924d5b3e218d9df914d8d7ee287af002525485
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2024-10-26 19:34:23 +0900

    [level 3] Title: 단어 변환, Time: 0.46 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,28 +1,43 @@
 from collections import deque
 def solution(begin, target, words):
     answer = 0
-    #변환할 수 없는 경우
-    if target not in words:
-        return answer
     
-    return bfs(begin, target, words)
+    #알파벤 하나만 다른지 체크해주는 함수
+    def check (a,b):
+        cnt = 0
+        for i in range(len(a)):
+            if a[i] != b[i]:
+                cnt+=1
+        if cnt == 1:
+            return True
+        else:
+            return False
     
-def bfs(begin, target, words):
-    
+    def bfs(b,target,answer):
         q = deque()
-        q.append([begin,0])
+        #begin 단어는 words에 없으므로 idx 값 0으로 삽입
+        q.append((b,0))
+        if target not in words:
+            return 0
+        visited = [0] * len(words)
         
         while(q):
-            now, step = q.popleft()
-            
-            if now == target:
-                return step
-            
-            for word in words:
-                cnt = 0
-                for i in range(len(word)):
-                    if word[i] != now[i]:
-                        cnt += 1
+            b,idx = q.popleft()
+            if b == target:
+                return visited[idx]
+            for i in range(len(words)):
+                #현재 글자와 하나만 다르고 방문한적 없는 글자이면
+                    if check(b,words[i]) and visited[i] == 0:
+                        q.append((words[i],i))
+                        visited[i] = visited[idx] + 1
                         
-                if cnt == 1:
-                    q.append([word,step+1])
+                        
+                        
+                    
+    answer = bfs(begin,target,answer)               
+            
+        
+    
+    return answer
+
+    
