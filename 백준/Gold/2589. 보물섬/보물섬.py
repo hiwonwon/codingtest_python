93fbae4d4be84c1f3ba29da7d12f0a93e4d51e05
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2024-10-28 22:15:11 +0900

    [Gold V] Title: 보물섬, Time: 900 ms, Memory: 119820 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.py
+++ 백준/Gold/2589. 보물섬/보물섬.py
@@ -1,38 +1,30 @@
-import sys
 from collections import deque
-input = sys.stdin.readline
 
-# 첫 번째 줄에서 n과 m을 읽음 (행의 수와 열의 수)
-n, m = map(int, input().split())
+Y, X = map(int, input().split())
+bomul_map = [list(input().rstrip()) for _ in range(Y)]
 
-# n개의 행을 지도 배열로 읽어들임
-arr = [list(input().strip()) for _ in range(n)]
+maxi = 0
+for y in range(Y):
+    for x in range(X):
+        if bomul_map[y][x] == 'L':
+            visited = [[0] * X for _ in range(Y)]
+            dist = [[0] * X for _ in range(Y)]
+
+            # BFS
+            q = deque()
+            q.append((y, x))
+            visited[y][x] = 1
+
+            while len(q) > 0:
+                c_y, c_x = q.popleft()
+                for dy, dx in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                    ny, nx = c_y + dy, c_x + dx
+                    if 0 <= ny < Y and 0 <= nx < X and bomul_map[ny][nx] == 'L':
+                        if visited[ny][nx] == 0:
+                            visited[ny][nx] = 1
+                            dist[ny][nx] = max(dist[c_y][c_x]+1, dist[ny][nx])
+                            maxi = max(maxi, dist[ny][nx])
+                            q.append((ny, nx))
 
 
-def bfs(a,b):
-    ans = [[0]*m for _ in range(n)]
-    q = deque()
-    q.append((a,b))
-    ans[a][b] = 1
-    mx = 0
-
-    dx = [-1,1,0,0]
-    dy = [0,0,-1,1]
-    while(q):
-        a,b = q.popleft()
-        for i in range(4):
-            x = dx[i] + a
-            y = dy[i] + b
-            #범위안에 있고 육지이고 아직 방문한적없는 칸이면
-            if 0<=x<n and 0<=y<m and arr[x][y] != 'W' and ans[x][y] == 0:
-                ans[x][y] = ans[a][b] + 1
-                mx = max(mx, ans[x][y])
-                q.append((x,y))
-    return mx-1
-
-ans = 0
-for i in range(n):
-    for j in range(m):
-        if arr[i][j] == "L":
-            ans = max(ans,bfs(i,j))
-print(ans)
+print(maxi)
