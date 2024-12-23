b216c45402171847e6a73c23870251b0bf8d481b
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2024-12-23 22:41:40 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 2884 ms, Memory: 34936 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
@@ -1,36 +1,44 @@
-#컨베이어: 길이가 n/ 벨트: 2n 
-#1 = 올리는 위치 / N= 내리는 위치
-#올리는 위치에 올릴때 이동할때 칸의 내구도 1 감소
+
+
+import sys
 from collections import deque
 
-ans = 0
+input = sys.stdin.readline
 
-n,k = map(int,input().split())
-a = deque(map(int,input().split()))
-robot = deque([False] * (n))
+N, K = map(int, input().split())
 
+belt = deque(list(map(int, input().split())))   # 매번돌리기, 칸별 내구도
 
-while (True):
-    ans += 1
-    
-    a.rotate(1)
-    robot.rotate(1)
+ridx = deque()  # 각 로봇들의 현재 인덱스
+cur = deque([0]*2*N)   # 각 칸별 로봇 존재 여부
 
-    robot[-1] = 0
-    
-    #내리는 위치의 전 위치에서부터 확인(가장 먼저 올려진 로봇부터 확인)
-    for i in range(n-2,-1,-1):
-        #현재 위치에 로봇이 있고 다음위치에 로봇이 없으며 다음 위치의 내구성이 1이상일 때
-        if robot[i] and not robot[i+1] and a[i+1] >=1:
-            robot[i],robot[i+1] = False, True
-            a[i+1] -= 1
-    robot[-1] = False
-    if a[0] >= 1:
-        robot[0] = True
-        a[0] -= 1 
+answer = 0
 
-    if a.count(0) >= k:
+while True:
+    answer += 1
+
+    belt.appendleft(belt.pop())     #한칸 회전
+    cur.appendleft(cur.pop())
+
+    if cur[N-1] == 1:               # 내리는 칸에 가면 로봇 내리기
+        cur[N-1] = 0
+
+    # 회전 후, 각 칸의 로봇 이동 결정
+    for i in range(N-2, -1, -1):    # 마지막(내리는칸)은 처리했으니 신경쓰지말기, 들어온순서대로, 위에칸만
+        if cur[i] == 1:     # 로봇이 존재하는 경우만 움직이기
+            if belt[i+1] >= 1 and cur[i+1] == 0:    # 다음칸에 로봇이없고, 내구도가있으면 움직이기
+                cur[i] = 0
+                cur[i+1] = 1
+                belt[i+1] -= 1
+
+    if cur[N-1] == 1:               # 내리는 칸에 가면 로봇 내리기
+        cur[N-1] = 0
+
+    if belt[0] >= 1:
+        cur[0] = 1
+        belt[0] -= 1
+
+    if belt.count(0) >= K:
         break
 
-print(ans)
-
+print(answer)
