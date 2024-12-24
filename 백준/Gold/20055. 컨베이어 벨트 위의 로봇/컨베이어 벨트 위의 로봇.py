109bbd9a4e9b67f54297a8967998f4268ddf0a18
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2024-12-24 22:04:54 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 1872 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
@@ -1,44 +1,41 @@
-
-
 import sys
 from collections import deque
 
-input = sys.stdin.readline
+def main(N,K,A):
+    A = deque(A)
+    robots = deque([False] * N)
 
-N, K = map(int, input().split())
+    steps = 0
+    while True:
+        steps += 1
 
-belt = deque(list(map(int, input().split())))   # 매번돌리기, 칸별 내구도
+        # 벨트와 로봇의 회전
+        A.rotate(1)
+        robots.rotate(1)
 
-ridx = deque()  # 각 로봇들의 현재 인덱스
-cur = deque([0]*2*N)   # 각 칸별 로봇 존재 여부
+        # N 번 컨데이너의 로봇을 내린다.
+        robots[N-1] = False
 
-answer = 0
+        # 가장 앞에있는 로봇부터 이동
+        for i in range(N-2, -1, -1):
+            if robots[i] and not robots[i+1] and A[i+1] >= 1:
+                robots[i], robots[i+1] = False, True
+                A[i+1] -= 1
+        
+        robots[N-1] = False
 
-while True:
-    answer += 1
+        if A[0] != 0:
+            robots[0] = True
+            A[0] -= 1
+        
+        if A.count(0) >= K:
+            break
+    
+    return steps
 
-    belt.appendleft(belt.pop())     #한칸 회전
-    cur.appendleft(cur.pop())
 
-    if cur[N-1] == 1:               # 내리는 칸에 가면 로봇 내리기
-        cur[N-1] = 0
+N, K = map(int, sys.stdin.readline().split())
+A = list(map(int, sys.stdin.readline().split()))
+res = main(N,K,A)
 
-    # 회전 후, 각 칸의 로봇 이동 결정
-    for i in range(N-2, -1, -1):    # 마지막(내리는칸)은 처리했으니 신경쓰지말기, 들어온순서대로, 위에칸만
-        if cur[i] == 1:     # 로봇이 존재하는 경우만 움직이기
-            if belt[i+1] >= 1 and cur[i+1] == 0:    # 다음칸에 로봇이없고, 내구도가있으면 움직이기
-                cur[i] = 0
-                cur[i+1] = 1
-                belt[i+1] -= 1
-
-    if cur[N-1] == 1:               # 내리는 칸에 가면 로봇 내리기
-        cur[N-1] = 0
-
-    if belt[0] >= 1:
-        cur[0] = 1
-        belt[0] -= 1
-
-    if belt.count(0) >= K:
-        break
-
-print(answer)
+print(res)
