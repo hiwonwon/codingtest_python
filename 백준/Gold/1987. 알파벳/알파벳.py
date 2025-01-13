e43456a50c1ce82713a4f755e17886f297240a21
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2025-01-13 22:38:49 +0900

    [Gold IV] Title: 알파벳, Time: 4880 ms, Memory: 166564 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,30 +1,22 @@
-from collections import deque
-import sys
+R, C = map(int,input().split())
+alp = list(input() for _ in range(R))
 
+V = [0]*128   
+V[ord(alp[0][0])] = 1
+ans = 1
 
-r, c = map(int, input().split())
-board = [list(input()) for _ in range(r)]
-
-
-dx = [-1,1,0,0]
-dy = [0,0,1,-1]
-
-def dfs(a,b,cnt):
+def move_count(y,x,count):
     global ans
-
-    ans = max(cnt,ans)
-    
+    ans = max(ans,count)
+    dy = [0,-1,0,1]
+    dx = [1,0,-1,0]
     for i in range(4):
-        nx = a + dx[i]
-        ny = b + dy[i]
-        if 0<= nx < r and 0<= ny < c:
-            if board[nx][ny] not in alpha:
-                alpha.add(board[nx][ny])
-                dfs(nx,ny,cnt+1)
-                alpha.remove(board[nx][ny])
+            ny = y + dy[i]
+            nx = x + dx[i]
+            if 0<=ny<R and 0<=nx<C and V[ord(alp[ny][nx])] == 0:
+                V[ord(alp[ny][nx])] = 1     
+                move_count(ny,nx,count+1)
+                V[ord(alp[ny][nx])] = 0
 
-
-ans = 0
-alpha= set(board[0][0])
-dfs(0,0,1)
+move_count(0,0,1)
 print(ans)
