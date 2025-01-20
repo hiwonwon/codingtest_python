1d06fa0ebd54fa595ea4535e0aaa3e430f70663e
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2025-01-20 18:16:08 +0900

    [Gold IV] Title: 플로이드, Time: 164 ms, Memory: 111556 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -1,28 +1,27 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 m = int(input())
-
-inf = float("INF")
-
-ans = [[inf]*(n+1) for _ in range(n+1)] 
-for i in range(1, n + 1):
-    for j in range(1, n + 1):
-        if i == j:
-            ans[i][j] = 0
-
-
-for _ in range(m):
+dist = [[float('inf')  for _ in range(n + 1)] for _ in range(n+1) ] 
+for i in range(m):
     a,b,c = map(int,input().split())
-    ans[a][b] = min(ans[a][b],c)
-
+    dist[a][b] = min(c, dist[a][b])
+for i in range(1,n+1):
+    dist[i][i] = 0
+    
 for k in range(1,n+1):
-    for i in range(1,n+1):
+    for i in range(1, n+1):
         for j in range(1,n+1):
-            ans[i][j] = min(ans[i][j],ans[i][k]+ans[k][j])
+            if i == j:
+                continue
+            if dist[i][j] > dist[i][k] + dist[k][j]:
+                dist[i][j] = dist[i][k] + dist[k][j]
 
-for a in range(1, n + 1):
-    for b in range(1, n + 1):
-        if ans[a][b] == inf:
-            print("0",  end=" ")
+for i in dist[1:]:
+    for j in i[1:]:
+        if j == float('inf'):
+            print(0, end=' ')
         else:
-            print(ans[a][b], end=" ")
+            print(j, end=' ')
     print()
