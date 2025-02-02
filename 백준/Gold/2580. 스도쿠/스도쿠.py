ccdc54adc3348528e8640f0bd97e80337d96990b
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2025-02-02 23:48:15 +0900

    [Gold IV] Title: 스도쿠, Time: 936 ms, Memory: 116300 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.py
+++ 백준/Gold/2580. 스도쿠/스도쿠.py
@@ -1,54 +1,46 @@
-import sys
-from collections import deque
-
-matrix = [list(map(int, sys.stdin.readline().split())) for _ in range(9)]
-
-# empty_spot : candidates list
-def get_spots(matrix):
-    empty_spots = []
-    for i in range(9):
-        for j in range(9):
-            if matrix[i][j] == 0:
-                empty_spots.append((i, j))
-    return empty_spots
-
-def test_candidate(x, y, i):
-    if i in matrix[x]:
-        return False
-    
-    for a in range(9):
-        if matrix[a][y] == i:
-            return False
-        
-    dxs = [ 2-x%3, 1-x%3, 0-x%3]
-    dys = [ 2-y%3, 1-y%3, 0-y%3]
-    for dx in dxs:
-        for dy in dys:
-            nx, ny = x + dx, y + dy
-            if matrix[nx][ny] == i:
+def dfs(table, verticals, horizontals, squares):
+    for row in range(9):
+        for col in range(9):
+            if table[row][col] == 0:
+                for num in range(1, 10):
+                    if not (verticals[col][num-1] or horizontals[row][num-1] or squares[(row//3)*3 + (col//3)][num-1]):
+                        horizontals[row][num-1] = verticals[col][num-1] = squares[(row//3)*3 + (col//3)][num-1] = True
+                        table[row][col] = num
+                        if dfs(table, verticals, horizontals, squares):
+                            return True
+                        table[row][col] = 0
+                        horizontals[row][num-1] = verticals[col][num-1] = squares[(row//3)*3 + (col//3)][num-1] = False
                 return False
     return True
-        
-
-# def find_candidates(x, y):
-#     for i in list(candidates[(x, y)]):
-#         if not test_candidate(x, y, i): # 값 가능하지않으면
-#             candidates[(x, y)].remove(i)
+            
 
 
-def fill_number(count, empty_spots):
-    if count == len(empty_spots):
-        for row in matrix:
-            print(*row)
-        exit()
-    x, y = empty_spots[count]
-    for i in range(1, 10):
-        if test_candidate(x, y, i):
-            matrix[x][y] = i
-            fill_number(count+1,empty_spots)
-            matrix[x][y] = 0
-        
+n = 9
+verticals = [[False]*n for _ in range(n)]
+horizontals = [[False]*n for _ in range(n)]
+squares = [[False]*n for _ in range(n)]
+table = []
+#print(verticals)
+
+for i in range(9):
+    arr = list(map(int, input().split()))  
+    table.append(arr)
+    for j in range(9):
+        if arr[j] != 0:
+            #행으로 봤을 때 어떤 원소가 부족한지를 알아내야 함.
+            #열로 봤을 때도 알아내야 하니까 horizontal이 필요한거임.
+            #table[i][j] = arr[j]
+            verticals[j][arr[j]-1] = True
+            horizontals[i][arr[j]-1] = True
+            t = (i//3)*3 + (j//3)
+            squares[t][arr[j]-1] = True
 
 
-empty_spots = get_spots(matrix)
-fill_number(0, empty_spots)
+#temp = dfs(table, verticals, horizontals, squares)
+
+
+if dfs(table, verticals, horizontals, squares):
+    for row in table:
+        for item in row:
+            print(item, end=' ')
+        print("")
