7f9da3e8015405c24da630b61ec8b209ad4080de
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2025-02-21 16:26:38 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 2484 ms, Memory: 35864 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
@@ -1,38 +1,26 @@
 import sys
-input = sys.stdin.readline
-#recursion error 방지
-sys.setrecursionlimit(10**9)
+sys.setrecursionlimit(10**6) # 최대 재귀 회수 확장
 
-arr = []
+array=[]
 while True:
     try:
-        x = int(input())
-        arr.append(x)
+        array.append(int(input()))
     except:
         break
-
-
-def solution(A):
-    # 받은 배열 길이가 0이면 return
-    if len(A) == 0:
-        return
-
-    tempL, tempR = [], []
-    # 첫번째 값을 루트 노드로 설정
-    mid = A[0]
-    # 나머지 배열에 대해 for문을 돌면서 루트보다 커지는 부분을 기록해서 L과 R로 나눈다.
-    for i in range(1, len(A)):
-        if A[i] > mid:
-            tempL = A[1:i]
-            tempR = A[i:]
-            break
-    else:
-    	#모두 mid보다 작은 경우
-        tempL = A[1:]
     
-    #왼쪽, 오른쪽 순으로 재귀 후 루트 노드 값 출력
-    solution(tempL)
-    solution(tempR)
-    print(mid)
-
-solution(arr)
+def dfs(start,end):
+    if start > end: # 오른쪽 자식 노드가 없는 경우 =>리프 노드인 경우우
+        return
+    right_start=end+1 # 오른쪽 노드가 없을 경우 대비해서 종료 조건 설정
+    
+    for i in range(start+1,end+1): # 서브 트리에서 루트 노드를 제외한 자식 노드 중에서 루트 노드 보다 큰 값을 찾음
+        if array[start]<array[i]:
+            right_start=i
+            break
+    
+    dfs(start+1,right_start-1) # 왼쪽 서브 트리 
+    dfs(right_start,end) # 오른쪽 서브 트리
+    print(array[start])
+        
+    
+dfs(0,len(array)-1)
