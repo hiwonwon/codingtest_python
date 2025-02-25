288def7312c4c70a972158fe913700eb92762d44
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2025-02-25 23:50:42 +0900

    [Gold IV] Title: LCS 2, Time: 212 ms, Memory: 57628 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.py
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -1,30 +1,38 @@
-import sys
-sys.setrecursionlimit(10**6)
-A = list(input())
-B = list(input())
-dp = [[0]*(len(B)+1) for i in range(len(A)+1)]
-path = []
+def LCS(s1, s2):
+    len1, len2 = len(s1), len(s2)
+    dp = [[0] * (len2 + 1) for _ in range(len1 + 1)]
 
-for i in range(1, len(A)+1):
-    for j in range(1, len(B)+1):
-        if A[i-1] == B[j-1]:
-            dp[i][j] = dp[i-1][j-1] + 1
-            continue
-        dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+    # DP 테이블 채우기
+    for i in range(1, len1 + 1):
+        for j in range(1, len2 + 1):
+            if s1[i - 1] == s2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
 
-print(dp[len(A)][len(B)])
+    # LCS 길이
+    lcs_length = dp[len1][len2]
+    
+    # LCS 문자열 복원
+    lcs = []
+    i, j = len1, len2
+    while i > 0 and j > 0:
+        if s1[i - 1] == s2[j - 1]:
+            lcs.append(s1[i - 1])
+            i -= 1
+            j -= 1
+        elif dp[i - 1][j] > dp[i][j - 1]:
+            i -= 1
+        else:
+            j -= 1
 
-def func(r, c):
-    if r == 0 or c == 0:
-        return 
-    if A[r-1] == B[c-1]:
-        path.append(A[r-1])
-        func(r-1, c-1)
-        return
-    if dp[r-1][c] > dp[r][c-1]:
-        func(r-1, c)
-        return
-    func(r, c-1)
+    lcs.reverse()  # 역순이므로 뒤집기
 
-func(len(A), len(B))
-print(*reversed(path), sep='')
+    print(lcs_length)
+    if lcs_length > 0:
+        print("".join(lcs))
+
+# 예제 실행
+s1 = input().strip()
+s2 = input().strip()
+LCS(s1, s2)
