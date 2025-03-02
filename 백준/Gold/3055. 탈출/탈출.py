33d5560b83b2357b455188411c03da8fae337297
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2025-03-02 23:08:20 +0900

    [Gold IV] Title: 탈출, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/3055. 탈출/탈출.py
+++ 백준/Gold/3055. 탈출/탈출.py
@@ -1,68 +1,53 @@
-from collections import deque
+import sys
+input = sys.stdin.readline
 
-r,c = map(int, input().split())
-graph = [list(input().strip()) for _ in range(r)]
-visited = [[0]*c for _ in range(r)]
+# 입력받기
+R, C = map(int, input().split())
+matrix = [list(input().strip()) for _ in range(R)]
 
-
-
-dx = [1,-1,0,0]
-dy = [0,0,-1,1]
-
-def bfs(si,sj):
-
-    visited[si][sj] = 1
-
-    while q:
-        x,y = q.popleft()
-
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0<= nx <r and 0<= ny <c:
-                if (graph[nx][ny] =='.' or graph[nx][ny] == "S") and graph[x][y] == '*':
-                        graph[nx][ny] = '*'
-                        q.append((nx,ny))
-
-                elif (graph[nx][ny] =='.' or graph[nx][ny] == "D") and graph[x][y] == 'S':
-                        visited[nx][ny] = visited[x][y] +1
-                        graph[nx][ny] = 'S'
-                        q.append((nx,ny))
-
-                
-
-
-q = deque()
-
-for i in range(r):
-    for j in range(c):
-        #고슴도치위치저장
-        if graph[i][j] == 'S':
-            si,sj = i,j
-            q.append((si,sj))
-
-for i in range(r):
-    for j in range(c):
-        #비버의 굴 위치 저장
-        if graph[i][j] == "D":
-            di = i
-            dj = j
-
-for i in range(r):
-    for j in range(c):
-        if graph[i][j] == "*":
-            wi = i
-            wj = j
-            q.append((wi,wj))
-
-
-
-
-bfs(si,sj)
-
-if visited[di][dj]:
-    print(visited[di][dj] - 1)
-else:
-    print("KAKTUS")
-    
+# *인 곳과 S인 곳 큐에 넣기 
+# 물이 차는 것이 비버의 이동 보다 먼저이다.
+queue = []
+for i in range(R) :
+    for j in range(C) :
+        if matrix[i][j] == '*' :
+            queue.append([i, j])
             
+for i in range(R) :
+    for j in range(C) :
+        if matrix[i][j] == 'S' :
+            # 고슴도치를 0 으로 변경 (시간 count 하기 위해)
+            matrix[i][j] = 0
+            queue.append([i, j])
+            
+dr = [1, -1, 0, 0]
+dc = [0, 0, 1, -1]
+
+# bfs
+def bfs() :
+    # 큐가 비기 전까지
+    while len(queue) > 0 :
+        # r, c 꺼내기
+        r, c = queue.pop(0)
+        
+        for i in range(4) :
+            nr = r + dr[i]
+            nc = c + dc[i]
+            # 경계 안에 있고 
+            if (nr >= 0 and nr < R and nc >= 0 and nc < C) :
+                # 고슴도치가 D에 도달하는 경우
+                if (matrix[r][c] != '*' and matrix[nr][nc] == 'D') :
+                    print(matrix[r][c] + 1)
+                    exit()
+                # 물이 이동하는 경우
+                if (matrix[r][c] == '*' and matrix[nr][nc] == '.') :
+                    matrix[nr][nc] = '*'
+                    queue.append([nr, nc])
+                # 고슴도치가 이동하는 경우
+                elif (matrix[r][c] != '*' and matrix[nr][nc] == '.') :
+                    matrix[nr][nc] = matrix[r][c] + 1
+                    queue.append([nr, nc])
+
+bfs()
+# 고슴도치가 갈 곳이 없는 경우
+print("KAKTUS")
