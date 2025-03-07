f6bbf1a72ab1f989bf2af154b313a03ae1da38bf
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2025-03-07 16:03:39 +0900

    [Gold IV] Title: 치즈, Time: 76 ms, Memory: 34976 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.py
+++ 백준/Gold/2636. 치즈/치즈.py
@@ -1,63 +1,47 @@
-import copy
+import sys
 from collections import deque
-r,c = map(int,input().split())
-graph = []
-for _ in range(r):
-    row = list(map(int,input().split()))
-    graph.append(row)
-dx = [0,0,1,-1]
-dy = [1,-1,0,0]
+input = sys.stdin.readline
 
 
-#구멍이 아닌 공기만 방문
-def bfs():
-    #판의 가장자리이므로 0,0은 항상 0임
-    q=deque([(0,0)])
+def cheeze():
+    ch_dq = deque()
+    ch_dq.append([0,0])
+    
+    melt_q = deque()
     visited[0][0] = 1
-
-    while(q):
-        x,y = q.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0<= nx<r and 0<=ny<c and visited[nx][ny] == 0 and graph[nx][ny] == 0:
+    
+    while ch_dq:
+        xx,yy = ch_dq.popleft()
+        
+        for nx,ny in ((xx+1,yy),(xx-1,yy),(xx,yy+1),(xx,yy-1)):
+            if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny]:
                 visited[nx][ny] = 1
-                q.append((nx,ny))
-pre = 0
-cnt = 0
-t = 0
-for g in graph:
-    cnt += g.count(1)
+                
+                if not lists[nx][ny]:
+                    ch_dq.append([nx,ny])
+                elif lists[nx][ny]:
+                    melt_q.append([nx,ny])
+    for x,y in melt_q:
+        lists[x][y] = 0
+    return len(melt_q)
+n,m = map(int,input().split())
+lists = [list(map(int,input().split())) for _ in range(n)]
+sum_cheeze = 0
+for x in lists:
+    sum_cheeze += sum(x)
 
+res = 0
 while True:
-    #치즈가 사라지면 종료
-    cnt = 0
-    for g in graph:
-        cnt += g.count(1)
-    if cnt == 0:
+    visited = [[0 for _ in range(m)] for _ in range(n)]
+
+    melt_cnt = cheeze()
+    sum_cheeze -= melt_cnt
+    
+    res += 1
+    if not sum_cheeze:
+        print(res)
+        print(melt_cnt)
         break
-    visited = [[0]*c for _  in range(r)]
-    #구멍을 제외한 공기만 방문하는 함수
-    bfs()
-    for i in range(r):
-        for j in range(c):
-            if graph[i][j] == 1:
-                #공기와 맞닿은 테두리 부분인지 체크
-                check = False
-                for k in range(4):
-                    nx = i + dx[k]
-                    ny = j + dy[k]
-                    if 0<= nx<r and 0<=ny<c and visited[nx][ny] == 1:
-                        check = True
-                        break
-                #테두리면 삭제 진행
-                if check:
-                    graph[i][j] = 0
-    pre = cnt
-    t += 1
+    
 
-
-print(t)
-print(pre)
-
-
+                
