53d60b9eb4922cad13bf04f540fe8b26a88d4196
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2025-03-16 18:41:40 +0900

    [Gold IV] Title: 팰린드롬?, Time: 2132 ms, Memory: 63888 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.py
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.py
@@ -1,27 +1,24 @@
 import sys
 input = sys.stdin.readline
+
 n = int(input())
-nums = list(map(int,input().split()))
-m = int(input())
+arr = [*map(int,input().split())]
 dp = [[0]*n for _ in range(n)]
-#길이가 1인 부분배열 처리
+
 for i in range(n):
     dp[i][i] = 1
-#길이가 2인 부분배열 처리
-for i in range(n-1):
-    if nums[i] == nums[i+1]:
-        dp[i][i+1] = 1
+    if i < n-1:
+        if arr[i] == arr[i+1]:
+            dp[i][i+1] = 1
+        else:
+            dp[i][i+1] = 0
 
-#cnt + 3 = 현재 확인하는 부분 배열의 길이
-for cnt in range(n-2):
-    for i in range(n-2-cnt):
-        #i가 현재 확인하는 부분배열의 시작점 j는 끝점
-        j = i+2+cnt
-        if nums[i] == nums[j] and dp[i+1][j-1] == 1:
-            dp[i][j] = 1
+for gap in range(2, n):
+    for i in range(n-gap):
+        if arr[i] == arr[i+gap] and dp[i+1][i+gap-1]:
+            dp[i][i+gap] = 1
 
-
+m = int(input())
 for _ in range(m):
-    s,e = map(int,input().split())
+    s, e = map(int,input().split())
     print(dp[s-1][e-1])
-
