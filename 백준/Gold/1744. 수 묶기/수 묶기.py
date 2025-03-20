c71f1dbc9dbaa4e792b26bfe7fe4671a85f737d5
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2025-03-20 15:40:06 +0900

    [Gold IV] Title: 수 묶기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/1744. 수 묶기/수 묶기.py
@@ -0,0 +1,45 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+plus = []
+one = []
+minus = []
+zero = False
+for _ in range(n):
+    tmp = int(input())
+    if tmp == 0 :
+        zero =True
+    elif tmp == 1:
+        one.append(1)
+    elif tmp >0 :
+        plus.append(tmp)
+    elif tmp < 0 :
+        minus.append(tmp)
+    
+
+#절댓값이 큰애들이 앞으로 오도록 
+plus.sort(reverse = True)
+minus.sort()
+
+ans = 0
+
+for i in range(len(plus)//2):
+    ans += plus[i*2]*plus[i*2+1]
+if len(plus)%2 == 1:
+    ans += plus[-1]
+for i in one:
+    ans += i
+
+
+for i in range(len(minus)//2):
+    ans += minus[i*2]*minus[i*2+1]
+#음수끼리 곱해서 양으로 만들 수 있는 경우 외에 음수가 하나 더있다면
+if len(minus)%2 == 1:
+    #0이 없다면 하나남은 음의 정수 빼줘야함
+    if not zero:
+        ans += minus[-1]
+
+
+print(ans)
