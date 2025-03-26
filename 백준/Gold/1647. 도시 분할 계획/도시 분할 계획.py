9f409c97d84769abddbf5f809ce8a6573f1b745e
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2025-03-26 15:45:41 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 2112 ms, Memory: 321436 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -1,32 +1,42 @@
-import itertools
-# 현재 모든집 서로 연결됨 어떤 임의 두집 골라도 연결됨
-n, m = map(int,input().split())
-edges = []
-for _ in range(m):
-    a,b,c = map(int,input().split())
-    edges.append([a,b,c])
+def main():
+    import sys
+    
+    lines = sys.stdin.read().rstrip().splitlines()
+    
+    N, M = map(int, lines[0].split())
+    edges = []
+    for i in range(1, M + 1):
+        a, b, weight = map(int, lines[i].split())
+        edges.append([a, b, weight])
+    edges.sort(key=lambda e: e[2], reverse=True)
+    
+    root = [x for x in range(N + 1)]
+    root[0] = 1
+    
+    def get_root(x):
+        nonlocal root
+        
+        if root[x] != x:
+            root[x] = get_root(root[x])
+        return root[x]
+    
+    sum_weight = 0
+    connected_components = N
+    while connected_components > 2:
+        a, b, weight = edges.pop()
+        
+        root_a = get_root(a)
+        root_b = get_root(b)
+        if root_a != root_b:
+            if root_a < root_b:
+                root[root_b] = root_a
+            else:
+                root[root_a] = root_b
+            sum_weight += weight
+            connected_components -= 1
+    
+    sys.stdout.write(str(sum_weight))
 
-edges.sort(key = lambda x:x[2])
 
-parent = [i for i in range(n+1)]
-
-def find(x):
-    if x!= parent[x]:
-        parent[x] = find(parent[x])
-    return parent[x]
-
-def union(a,b):
-    a = find(a)
-    b = find(b)
-
-    if a < b:
-        parent[b] = a
-    else:
-        parent[a] = b
-
-span = []
-for a,b,c in edges:
-    if find(a) != find(b):
-        union(a,b)
-        span.append(c)
-print(sum(span)-max(span))
+if __name__ == '__main__':
+    main()
