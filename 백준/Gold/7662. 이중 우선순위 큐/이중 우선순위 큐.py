1dc0b051013de4ec7bc22acf1bfc6008431056e7
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2025-03-29 21:11:56 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2072 ms, Memory: 290144 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
@@ -1,49 +1,60 @@
 import sys
 import heapq
 
-input = sys.stdin.readline
-t = int(input())
+def isEmpty(nums):
+    for item in nums:
+        if item[1] > 0:
+            return False
+    return True
 
-for _ in range(t):
-    visit = [False] * 1_000_001
-    max_heap, min_heap= [],[]
+t = int(sys.stdin.readline())
 
-    k = int(input())
-
-    for key in range(k):
-        order = input().rsplit()
-
-        if order[0] == 'I':
-            heapq.heappush(max_heap,(int(order[1])*-1,key))
-            heapq.heappush(min_heap, (int(order[1]),key))
-            visit[key] = True
-
-        elif order[0] == 'D':
-            if order[1] == '1':
-                while max_heap and not visit[max_heap[0][1]]:
-                        heapq.heappop(max_heap)
-                if max_heap:
-                    visit[max_heap[0][1]] = False
-                    heapq.heappop(max_heap)
-            elif order[1] == '-1':
-                while min_heap and not visit[min_heap[0][1]]:
-                        heapq.heappop(min_heap)
-                if min_heap:
-                    visit[min_heap[0][1]] = False
-                    heapq.heappop(min_heap)
-    while min_heap and not visit[min_heap[0][1]]:
-        heapq.heappop(min_heap)
-    while max_heap and not visit[max_heap[0][1]]:
-        heapq.heappop(max_heap)
-
-    if max_heap and min_heap:
-        print(-max_heap[0][0], min_heap[0][0])
-    else:
-        print("EMPTY")
-
-
+for i in range(t):
+    min_heap = []
+    max_heap = []
+    nums = dict()
+    k = int(sys.stdin.readline())
+    
+    for j in range(k):
+        oprt, oprd = sys.stdin.readline().split()
+        num = int(oprd)
+        
+        if oprt == 'I':
+            # 중복 삽입일 때
+            if num in nums:
+                nums[num] += 1
+            # 처음 삽입일 때
+            else:
+                nums[num] = 1
+                # min_heap에 추가
+                heapq.heappush(min_heap, num)
+                # max_heap에 추가
+                heapq.heappush(max_heap, -num)
                 
-
-
-
-
+        elif oprt == 'D':
+            # 큐가 비어있지 않을 때만
+            if not isEmpty(nums.items()):
+                # 최댓값을 제거
+                if num == 1:
+                    while -max_heap[0] not in nums or nums[-max_heap[0]] < 1:
+                        temp = -heapq.heappop(max_heap)
+                        if temp in nums:
+                            del(nums[temp])
+                    nums[-max_heap[0]] -= 1
+                # 최솟값을 제거
+                else:
+                    while min_heap[0] not in nums or nums[min_heap[0]] < 1:
+                        temp = heapq.heappop(min_heap)
+                        if temp in nums:
+                            del(nums[temp])
+                    nums[min_heap[0]] -= 1
+     
+    # 결과 출력           
+    if isEmpty(nums.items()):
+        print('EMPTY')
+    else:
+        while min_heap[0] not in nums or nums[min_heap[0]] < 1:
+            heapq.heappop(min_heap)
+        while -max_heap[0] not in nums or nums[-max_heap[0]] < 1:
+            heapq.heappop(max_heap)
+        print(-max_heap[0], min_heap[0])
