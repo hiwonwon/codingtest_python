9671ca4c388c0fc58b5253c151faadf0c5c0fcd1
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2025-04-06 23:33:58 +0900

    [Gold IV] Title: 오큰수, Time: 836 ms, Memory: 183880 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.py
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -1,14 +1,13 @@
-n = int(input())
-a = list(map(int,input().split()))
+N=int(input())
+A=list(map(int, input().split()))
 
-nge = [-1] * n
-stack = [0]
+stack=[]
+result=[-1]*N
 
-for i in range(1,n):
-    #stack[-1] = 현재 수 
-    while stack and a[stack[-1]] < a[i]:
-        nge[stack.pop()] = a[i] #오큰수를 찾았으므로 해당 위치 값 등록
-    
+for i in range(N):
+    while stack and A[stack[-1]]<A[i]:
+        result[stack.pop()]=A[i]
     stack.append(i)
+    
 
-print(*nge)
+print(" ".join(map(str,result)))
