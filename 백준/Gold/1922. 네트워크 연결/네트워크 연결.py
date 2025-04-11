7751190cd4a661aed0b31aaa1ae60b5f027540c3
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2025-04-11 14:08:33 +0900

    [Gold IV] Title: 네트워크 연결, Time: 288 ms, Memory: 53140 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
@@ -1,43 +1,41 @@
-#컴퓨터와 컴퓨터를 직접연결, 모든컴퓨터가 연결되어야함
-#연결하는 비용을 최소로
-#모두 연결못하는 경우 X
-
-#1.모두를 연결하면서 2. 비용이 최소가 되도록하는 선들을 구하는 것
-#알고리즘: N<=1000 이므로 이중 for 문까지 가능,최소신장트리!!!
-def find_parent(x):
-    if parent[x] != x:
-        parent[x] = find_parent(parent[x])
-    return parent[x]
-
-def union_parent(a,b):
-    a = find_parent(a)
-    b = find_parent(b)
-
-    if a<b:
-        parent[b] = a
-    else:
-        parent[a] = b
-
-
+import sys
+input = sys.stdin.readline
+from queue import PriorityQueue
 
 N = int(input())
 M = int(input())
+edges = PriorityQueue()
+parent = [x for x in range(N+1)]
 
-edges = []
 for _ in range(M):
-    a,b,c = map(int,input().split())
-    edges.append([c,a,b])
+    a, b, c = map(int, input().split())
+    edges.put((c, a, b))
 
-edges.sort()
-parent = [0] * (N+1)
-for i in range(1,N+1):
-    parent[i] = i
-total = 0
-for edge in edges:
-    price,a,b = edge
-    #사이클발생시키지 않는 경우 최소신장트리에 포함
-    if find_parent(a) != find_parent(b):
-        union_parent(a,b)
-        total += price
+def find(x):
+    
+    if parent[x] == x:
+        return x
+    else:
+        parent[x] = find(parent[x])
+        return parent[x]
 
-print(total)
+def union(a, b):
+    
+    a = find(a)
+    b = find(b)
+    
+    if a != b:
+        parent[b] = a
+
+edgenum = 0
+ans = 0
+
+while edgenum < N-1:
+    c, a, b = edges.get()
+    
+    if find(a) != find(b):
+        union(a, b)
+        ans += c
+        edgenum += 1
+
+print(ans)
