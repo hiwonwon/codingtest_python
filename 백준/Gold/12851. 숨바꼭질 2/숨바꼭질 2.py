9dad102827d02be32ddcb3f3d2c53364dc6218d6
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2025-04-14 16:45:02 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 132 ms, Memory: 114736 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
@@ -1,30 +1,20 @@
-#수빈 1초 후 x-1,x+1,2*x 범위 10만-> 이중for문 가능
-#가장 빠른시간, 방법의 수
-#알고리즘: bfs? 
+from collections import defaultdict, deque
 
-import sys
-input = sys.stdin.readline
-from collections import deque
-
-n, k = map(int,input().split())
-visit = [0] * 100_001
-
-q = deque()
-q.append(n)
-ans = 0
-cnt = 0
-
-while(q):
-    x = q.popleft()
-    if x == k:
-        cnt +=1
-        continue
-
-    for nx in x+1,x-1,x*2:
-        if 0<= nx <100_001:
-            if visit[nx]==0 or visit[nx] >= visit[x]+1:
-                q.append(nx)
-                visit[nx] = visit[x] + 1
-
-print(visit[k])
-print(cnt)
+N, K=map(int, input().split())
+dq=deque([N])
+visit=[float('inf')]*100001
+count=[0]*100001
+visit[N]=0
+count[N]=1
+while dq:
+    x=dq.popleft()
+    for y in [x-1, x+1, 2*x]:
+        if 0<=y<=100000 :
+            if visit[y]>visit[x]+1:
+                visit[y]=visit[x]+1
+                count[y]=count[x]
+                dq.append(y)
+            elif visit[y]==visit[x]+1:
+                count[y]+=count[x]
+print(visit[K])
+print(count[K])
