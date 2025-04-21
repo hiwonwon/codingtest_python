bcdc7f6305f47a5142bf5afdb51c3ace8d444917
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2025-04-21 23:34:50 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 424 ms, Memory: 65148 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.py
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.py
@@ -1,16 +1,23 @@
-n, m = map(int,input().split())
-graph = list(input() for i in range(n))
+from sys import stdin
 
-dp = [[0]*m for _ in range(n)]
-ans = 0
-for i in range(n):
-    for j in range(m):
-        if i == 0 or j == 0:
-            dp[i][j] = int(graph[i][j])
-        elif graph[i][j] == '0':
-            dp[i][j] = 0
-        else:
-            dp[i][j] = min(dp[i-1][j-1],dp[i-1][j],dp[i][j-1])+1
-        ans = max(ans,dp[i][j])
+def _1915(n, m, arr):
+    l = 0
+    dp = [[0] * (m+1) for _ in range(n+1)]
 
-print(ans**2)
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            if arr[i-1][j-1] == 1:
+                dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
+                if l < dp[i][j]:
+                    l = dp[i][j]
+    
+    print(l ** 2)
+
+if __name__ == "__main__":
+    input = stdin.readline
+    arr = []
+    n, m = map(int, input().split())
+    for _ in range(n):
+        arr.append(list(map(int, list(input().strip()))))
+
+    _1915(n, m, arr)
