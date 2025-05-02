00e208fc6e62fa401e714ecea2e653abb85697b1
hiwonwon <91616435+hiwonwon@users.noreply.github.com>
2025-05-02 23:52:03 +0900

    [Gold IV] Title: 가르침, Time: 404 ms, Memory: 190896 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.py
+++ 백준/Gold/1062. 가르침/가르침.py
@@ -1,47 +1,36 @@
-from itertools import combinations
-import sys
-
-n, k = map(int,input().split())
-
-words = [set(sys.stdin.readline().rstrip()) for _ in range(n)]
-if k < 5 :
-    print(0)
-    exit(0)
-elif k == 26:
-    print(n)
-    exit(0)
-
-learn = [0] * 26
-#꼭 배워야하는 5개 글자 처리
-for a in ('a', 'c', 'i', 'n', 't'):
-    learn[ord(a)-ord('a')] = 1
-
-
-ans = 0
-
-def dfs(idx,cnt):
-    global ans
-    
-    if cnt == k-5:
-        tmp = 0
-        for word in words:
-            #현재 조합 = 새로배우는 글자 + antatica 알파벳 중에 속하는지 확인
-            for a in word:
-                if learn[ord(a)-ord('a')] == 0:
-                    break
-            else:
-                #현재 단어에 필요한 알파벳이 현재 조합에 다 있다면 개수 추가
-                tmp += 1
-        if tmp > ans:
-            ans = tmp
+def dfs(start, depth, arr):
+    if depth == k - 5:
+        combs.append(arr[:])
         return
     
-    for i in range(idx,26):
-        if not learn[i]:
-            learn[i] = 1
-            dfs(i,cnt+1)
-            learn[i] = 0
-        
+    for i in range(start, len(alph)):
+        arr.append(alph[i])
+        dfs(i + 1, depth + 1, arr)
+        arr.pop()
 
-dfs(0,0)
+def word_to_bit(word):
+    bit = 0
+    for s in word:
+        bit |= (1 << ord(s) - ord('a'))
+    return bit
+
+n, k = map(int, input().split())
+words = [input() for _ in range(n)]
+bits = list(map(word_to_bit, words))
+base_bit = word_to_bit('antic')
+
+ans = 0
+if k >= 5:
+    alph = [1 << i for i in range(26) if not base_bit & 1 << i]
+    combs = []
+    dfs(0, 0, [])
+
+    for comb in combs:
+        learned_bit = sum(comb) | base_bit
+        cnt = 0
+        for bit in bits:
+            if bit & learned_bit == bit:
+                cnt += 1
+        ans = max(ans, cnt)
+
 print(ans)
